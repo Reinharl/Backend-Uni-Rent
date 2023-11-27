f00c27b0f2ac3aa6e5bf49ebbf7fe67bd6eeadec
Mirage <f.depinto00@gmail.com>
2023-11-27 16:27:37 +0100

    update CustomerRepository

--- src/main/java/com/unirent/repository/CustomerRepository.java
+++ src/main/java/com/unirent/repository/CustomerRepository.java
@@ -2,21 +2,12 @@ package com.unirent.repository;
 
 import com.unirent.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.List;
-
 @CrossOrigin("http://localhost:4200")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    
-    // Esempio di query personalizzata per trovare un cliente per email
-    @Query("SELECT c FROM Customer c WHERE c.email = :email")
+
     Customer findByEmail(@Param("email") String email);
 
-    // Esempio di query personalizzata per trovare tutti i clienti con un certo cognome
-    List<Customer> findByLastName(String lastName);
-
-    // Altre query personalizzate, se necessario 
 }

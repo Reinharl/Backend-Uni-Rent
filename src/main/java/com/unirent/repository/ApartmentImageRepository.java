bffe703859caad1b8820000524a4bfbc9953de8a
Mirage <f.depinto00@gmail.com>
2023-12-08 11:59:11 +0100

    expose new endpoint for ApartmentImage

--- src/main/java/com/unirent/repository/ApartmentImageRepository.java
+++ src/main/java/com/unirent/repository/ApartmentImageRepository.java
@@ -3,7 +3,13 @@ package com.unirent.repository;
 import com.unirent.entity.ApartmentImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "apartment-images", path = "apartment-images")
 public interface ApartmentImageRepository extends JpaRepository<ApartmentImage, Long> {
+
+    @RestResource(path = "byApartmentId")
+    List<ApartmentImage> findByApartment_Id(Long id);
 }

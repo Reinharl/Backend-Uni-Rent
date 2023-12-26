4c0807afc46b9766b0bac2d1a7b008c454945cd8
Mirage <f.depinto00@gmail.com>
2023-12-26 13:15:48 +0100

    Add logging and CORS support to CustomerController
    This commit introduces logging for debugging purpose using the Slf4j library in the CustomerController class. In addition, it adds CORS support and allows requests from the specified origin (http://localhost:5173) in order to handle cross-domain security constraints.

--- src/main/java/com/unirent/controller/CustomerController.java
+++ src/main/java/com/unirent/controller/CustomerController.java
@@ -2,21 +2,21 @@ package com.unirent.controller;
 
 import com.unirent.entity.Customer;
 import com.unirent.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/customers")
+@CrossOrigin("http://localhost:5173")
 public class CustomerController {
 
     @Autowired
@@ -39,8 +39,12 @@ public class CustomerController {
 
         String chatUrl = "http://localhost:8080/chat?prompt=" + prompt;
 
+        log.info(prompt);
+
         try {
             String chatResponse = restTemplate.getForObject(chatUrl, String.class);
+
+            log.info(chatResponse);
 
             Long id = customerService.findFirstNumberInResponse(chatResponse);
 
